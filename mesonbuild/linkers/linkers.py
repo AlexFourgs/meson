efbb9e9a343f8497fcd059a924b102e518fd9f21
AlexFourgs <alexandre.fourgs@gmail.com>
2021-12-07 10:47:19 +0100

    fixup! compiler: add support for DSP SHARC

--- mesonbuild/linkers/linkers.py
+++ mesonbuild/linkers/linkers.py
@@ -321,7 +321,7 @@ class SharcLinker(StaticLinker):
         return False
 
     def get_output_args(self, target: str) -> T.List[str]:
-        return [f'-o{target}']
+        return [f'{target}']
 
 
 
@@ -1494,11 +1494,34 @@ class SharcDynamicLinker(DynamicLinker):
         super().__init__(['cc21k'], for_machine, '', [],
                          version=version)
 
+    #def get_link_whole_for(self, args: T.List[str]) -> T.List[str]:
+    #    if not args:
+    #        return args
+    #    return self._apply_prefix('-Wl,--whole-archive') + args + self._apply_prefix('-Wl,--no-whole-archive')
+
     def get_accepts_rsp(self) -> bool:
         return False
 
-    def get_std_shared_lib_args(self) -> 'T.NoReturn':
-        raise MesonException('The Sharc Linkers do not support shared libraries')
+    def get_lib_prefix(self) -> str:
+        return ''
+
+    def get_std_shared_lib_args(self) -> T.List[str]:
+        return []
+
+    def get_output_args(self, outputname: str) -> T.List[str]:
+        return [f'-o{outputname}']
+
+    def get_search_args(self, dirname: str) -> T.List[str]:
+        return [f'-L{dirname}']
 
     def get_allow_undefined_args(self) -> T.List[str]:
         return []
+
+    def get_soname_args(self, env: 'Environment', prefix: str, shlib_name: str,
+                        suffix: str, soversion: str, darwin_versions: T.Tuple[str, str]) -> T.List[str]:
+        raise MesonException(f'{self.id} does not support shared libraries.')
+
+    def build_rpath_args(self, env: 'Environment', build_dir: str, from_dir: str,
+                         rpath_paths: str, build_rpath: str,
+                         install_rpath: str) -> T.Tuple[T.List[str], T.Set[bytes]]:
+        return ([], set())
